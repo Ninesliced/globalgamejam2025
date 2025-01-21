a7ba1e669a2fd7aefe2551f329d90cd498fd191a
Guillame Tran <guillaume1.tran@epitech.eu>
2025-01-21 15:22:27 +0100

    ADD player movement with acceleration and decceleration

--- scenes/actors/player/components/movement_controller.gd
+++ scenes/actors/player/components/movement_controller.gd
@@ -21,7 +21,14 @@ func _physics_process(delta):
 
 
 func handle_movement(delta,velocity):
+	var new_velocity = Vector2(0, 0)
 	var vec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var new_velocity = velocity.move_toward(vec * player.speed, player.acceleration * delta)
+
+	vec = vec.normalized()
+
+	if vec != Vector2(0, 0):
+		new_velocity = velocity.move_toward(vec * player.speed, player.acceleration * delta)
+	else:
+		new_velocity = velocity.move_toward(Vector2(0, 0), player.friction * delta)
+	
 	return new_velocity
-	pass
