a19612a0102c9b8a954902b2dced8eff8e6d588a
Guillame Tran <guillaume1.tran@epitech.eu>
2025-01-21 15:09:46 +0100

    ADDED player movement controller base

--- scenes/actors/player/components/movement_controller.gd
+++ scenes/actors/player/components/movement_controller.gd
@@ -1,11 +1,28 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
+var player : Player = null
+var velocity = Vector2(0, 0)
+var speed = 0
 func _ready():
+	var parent = get_parent()
+	if parent is Player:
+		player = parent
+		speed = player.speed
+	else:
+		assert(false, "MovementController must be a child of Player")
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func _physics_process(delta):
+	velocity = handle_movement()
+	player.velocity = velocity
+
+
+
+func handle_movement():
+	var vec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = vec * speed
+	return velocity
 	pass
--- scenes/actors/player/player.gd
+++ scenes/actors/player/player.gd
@@ -7,8 +7,4 @@ func _ready():
     pass
 
 func _process(delta):
-    var vec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
-    velocity = vec * speed
-
     move_and_slide()
