fbdd80d7857b93c598a0d4bfab0eedebebf0dc9d
ARKANYOTA <arkanyota@icloud.com>
2025-01-21 21:08:11 +0100

    scroll map

--- scenes/levels/map.gd
+++ scenes/levels/map.gd
@@ -6,19 +6,31 @@ var next_to_load_is_level = false
 var PackedScenelevel: PackedScene = preload("res://scenes/levels/level.tscn")
 var PackedScenebetween_level: PackedScene = preload("res://scenes/levels/between_level.tscn")
 
+var camera_depth = 0
+var next_level_position = 0
+var current_generate_level = 0
+
+var current_level = 0:
+	set(value):
+		current_level = value
+		print(current_level)
+		
+var is_on_a_level = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_next_level()
 	load_next_level()
 	load_next_level()
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if len(levels) >= 3:
-		if levels[0].position.y + get_size_of_level(levels[0]).y < 0:
+		var topLeft = $Camera2D.get_screen_center_position() - get_viewport_rect().size / 2  
+		if levels[0].position.y + get_size_of_level(levels[0]).y < topLeft.y:
 			load_next_level()
 			print("Next level is loading")
 	if Input.is_action_pressed("test_arkanyota"):
@@ -34,8 +46,11 @@ func load_next_level():
 	
 	if next_to_load_is_level:
 		node = PackedScenelevel.instantiate()
+		node.associated_level = current_generate_level
 	else:
 		node = PackedScenebetween_level.instantiate()
+		node.associated_level = current_generate_level
+		current_generate_level += 1
 
 	if len(levels) != 0:
 		var size = get_size_of_level(levels[-1])
@@ -61,10 +76,9 @@ func get_size_of_level(level_node):
 	return size
 	
 func down_the_screen():
-	print(levels)
-	for level in levels:
-		level.set_position(level.get_position() - Vector2(0,1))
-
-
+	# for level in levels:
+	# 	level.set_position(level.get_position() - Vector2(0,1)
+	$Camera2D.position.y += 1
+	
 
 	
