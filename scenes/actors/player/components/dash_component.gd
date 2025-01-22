f5e96f8f594760db5242f5ce0f0dfdb3276c50a9
Guillame Tran <guillaume1.tran@epitech.eu>
2025-01-22 12:13:42 +0100

    ENHANCE dash component with distance and duration parameters, and implement dashing logic

--- scenes/actors/player/components/dash_component.gd
+++ scenes/actors/player/components/dash_component.gd
@@ -4,7 +4,13 @@ var player : Player = null
 var velocity = Vector2(0, 0)
 
 @export var dash_consumption = 10
-@export var dash_force = 1000
+@export var dash_distance = 10000
+@export var dash_duration = 0.5
+
+var _dash_direction = Vector2(0, 0)
+var is_dashing = false
+var dash_timer : Timer = Timer.new()
+
 @export var dash_mode : Global.PlayMode = Global.PlayMode.EIGHT_WAY
 @export var oxygen_component : OxygenComponent = null
 @export var minimum_dash_distance = 50
@@ -19,6 +25,11 @@ func _ready():
 		assert(false, "dash component must be a child of Player")
 	if oxygen_component == null:
 		assert(false, "dash component must have an oxygen component")
+
+	dash_timer.wait_time = dash_duration
+	dash_timer.set_one_shot(true)
+	add_child(dash_timer)
+	dash_timer.timeout.connect(_disable_dash)
 	pass
 
 
@@ -26,19 +37,26 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	handle_dash()
+	_dash_direction = handle_dash()
+	if is_dashing:
+		player.velocity = ((_dash_direction * dash_distance) / dash_duration) * delta
 	pass
 
-func handle_dash() -> void:
-	if Input.is_action_just_pressed("dash"):
-		player.velocity = dash(player.velocity)
-		if player.velocity == Vector2.ZERO:
-			return
+func handle_dash():
+	if Input.is_action_just_pressed("dash") and not is_dashing:
+		_dash_direction = get_dash_direction(player.velocity)
+		if _dash_direction == Vector2.ZERO:
+			return Vector2.ZERO
 		oxygen_component.add_oxygen(-dash_consumption)
 		on_dash.emit()
+
+		is_dashing = true
+		dash_timer.start()
+		return _dash_direction
+	return _dash_direction
 		
 
-func dash(velocity) -> Vector2:
+func get_dash_direction(velocity) -> Vector2:
 	var vec = Vector2(0, 0)
 	if player.play_mode == Global.PlayMode.MOUSE:
 		var mouse_position = get_global_mouse_position()
@@ -51,5 +69,8 @@ func dash(velocity) -> Vector2:
 		if vec == Vector2.ZERO:
 			vec = Vector2(1,0)
 		vec = vec.normalized()
-	return vec * dash_force
+	return vec
+
+func _disable_dash():
+	is_dashing = false
 	
