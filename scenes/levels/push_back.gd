4b9a38f2c00b8ca5610b0c520d3109c11e974ba7
Yann Malaret <yann.malaret@epitech.eu>
2025-01-22 12:38:37 +0100

    [FIX] use tween for player push back

--- scenes/levels/push_back.gd
+++ scenes/levels/push_back.gd
@@ -1,23 +1,13 @@
 extends Area2D
 
-var player : Node2D = null
-var push_direction = Vector2.ZERO
-var push_timer = 0.0
-var push_duration = 0.5
-var push_distance = 300
+var push_distance = 200
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		push_direction = (global_position - body.global_position).normalized()
-		push_timer = push_duration
-		player = body
-
-func _process(delta: float) -> void:
-	if push_timer > 0:
-		push_timer -= delta
-		var push_step = push_distance * (delta / push_duration)
-		player.global_position += push_direction * push_step
+		var tween = get_tree().create_tween()
+		var push_direction = (global_position - body.global_position).normalized()
+		var target_position = body.global_position + push_direction * push_distance
+		tween.tween_property(body, "global_position", target_position, 0.5)
