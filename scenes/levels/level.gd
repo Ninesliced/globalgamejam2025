785fa96bceab1a82a173e167fc7d537a8e1143bf
ARKANYOTA <arkanyota@icloud.com>
2025-01-22 16:07:34 +0100

    next level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,17 +7,34 @@ class_name Level
 
 var level_has_been_passed = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_area_2d_body_entered(body):
 	if body is Player:
-		get_parent().get_parent().current_level = associated_level
-		get_parent().get_parent().is_on_a_level = true
+		get_parent().get_parent().next_current_level = associated_level
+		get_parent().get_parent().next_is_on_a_level = true
+
+func get_size_of_level(level_node):
+	var size
+	var LastLevelColisionShape = level_node.get_node("Area2D/CollisionShape2D")
+	var LastLevelColisionShape_Shape = LastLevelColisionShape.get_shape()
+	if LastLevelColisionShape_Shape is RectangleShape2D:
+		size = LastLevelColisionShape_Shape.size
+	else:
+		size = Vector2(2,2)
+		print("Is not a fucking shape")
+	return size
+
+
+
+func _on_area_2d_body_exited(body):
+	if body is Player:
+		get_parent().get_parent().current_level = get_parent().get_parent().next_current_level
+		get_parent().get_parent().is_on_a_level = get_parent().get_parent().next_is_on_a_level
 		level_has_been_passed = true
+		# var camera = get_parent().get_parent().camera
+		# var player = get_parent().get_parent().player
+		# camera.put_y(player.global_position.y + get_viewport_rect().size.y / 2)
