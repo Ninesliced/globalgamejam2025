a7b65b9f5f6fd0c72c799f79c7fd80b0e3b1afc5
Alexis Belmonte <alexbelm48@gmail.com>
2025-01-23 03:01:44 +0100

    scenes.actors.enemies.impl.enemy_octopus: wow it rotatE FASTER BANANAa

--- scenes/actors/components/movements/sine_movement_component.gd
+++ scenes/actors/components/movements/sine_movement_component.gd
@@ -1,4 +1,4 @@
-extends Component
+extends MovementComponent
 
 var direction_vector: Vector2
 var orthogonal_vector: Vector2
@@ -12,7 +12,7 @@ var time = 0.0
 
 func _ready():
 	super()
-
+	
 	direction_vector = Vector2.RIGHT.rotated(direction_angle)
 	orthogonal_vector = direction_vector.rotated(PI/2)
 
@@ -20,5 +20,4 @@ func _process(delta):
 	time = fmod(time + delta * (TAU / sine_wavelength), TAU)
 	var sine_value = sin(time) * sine_amplitude
 
-	assert(get_parent() is CharacterBody2D, "Parent is not a CharacterBody2D")
-	get_parent().velocity = orthogonal_vector * sine_value + direction_vector * movement_speed
+	parent.velocity = orthogonal_vector * sine_value + direction_vector * movement_speed
