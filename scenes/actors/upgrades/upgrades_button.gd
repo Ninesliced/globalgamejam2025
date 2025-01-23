16401d71a682eb6bc843e414c309357f5cd6ffc3
ARKANYOTA <arkanyota@icloud.com>
2025-01-23 06:42:22 +0100

    remove space on das.h on upgrade name

--- scenes/actors/upgrades/upgrades_button.gd
+++ scenes/actors/upgrades/upgrades_button.gd
@@ -9,7 +9,7 @@ var bbc_default_string = "\n[outline_size=20][b][center][wave]{text}[/wave][/cen
 var win_upgrades = {
 	"Increase oxygen capacity": win_oxygen_capacity,
 	"Refill the oxygen": refill_oxygen,
-	"The das h costs less": dash_cost_less,
+	"The dash costs less": dash_cost_less,
 	"The shoot costs less": shoot_cost_less,
 	"Your speed is faster": speed_fastest
 }
